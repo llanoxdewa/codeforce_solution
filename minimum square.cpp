1cbf9a13269b9328109cd035ddb1eed3f699f5ce
llanoxdewa <llanoxdew4@gmail.com>
2021-06-12 11:07:03 +0700

    my solution for minimum square

---
+++ minimum square.cpp
@@ -0,0 +1,59 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <math.h>
+#include <set>
+using namespace std;
+
+// debungging
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " -> ";_print(x);cerr << endl;
+#define debugArr(x,y) cerr << #x << " -> ";_print(x,y);cerr << endl;
+#else
+#define debug(x)
+#define debugArr(x,y)
+#endif
+// debugging templates
+template<typename T> void _print(T arg){cerr << arg << " ";}
+template<typename V> void _print(vector<V> arg){cerr << "[ ";for(V bil : arg) cerr << bil << " | ";cerr << "]";}
+template<typename K,typename V> void _print(map<K,V> arg){cerr << "{ ";for(auto &val : arg) cerr << val.first << " => " << val.second << " | ";cerr << "}";}
+template<typename A> void _print(A args[],int len){cerr << "[ ";for(int i = 0;i < len;i++)cerr << args[i] << " | ";cerr << "]";}
+
+// definition of data type
+#define ll long long
+#define ull unsigned long long
+
+
+
+
+void solve(){
+	int a,b;
+	int p,l;	
+	cin >> a >> b;
+	p = a*2;
+	l = b*2;
+	if(p > l){
+		cout << ((a>l)? a*a : l*l) << endl;
+		return;	
+	}	
+	cout << ((b>p)? b*b : p*p) << endl;
+}
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	#ifndef ONLINE_JUDGE
+		freopen("dbg.txt","w",stderr);
+   	#endif
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
+	
+	return 0;
+}
