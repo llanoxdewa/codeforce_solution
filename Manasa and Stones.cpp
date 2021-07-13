805e066317ac0dd54e5821db1b2648eb049df733
llanoxdewa <llanoxdew4@gmail.com>
2021-07-13 11:12:16 +0700

    my solution for Manasa and Stones

---
+++ Manasa and Stones.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+#include <set>
+#include <string>
+#include <map>
+
+using namespace std;
+
+// debugging
+//#include "testing_unit/debug.h"
+// shorcut
+#define ll long long
+#define ull unsigned long long
+#define print(val) cout << val << '\n';
+#define all(arr) arr.begin(),arr.end()
+
+void solve(){
+	int a,b,N;
+	cin >> N >> a >> b;
+	if(a > b)
+		swap(a,b);
+	set<int> res;	
+	for(int i = 0;i < N;i++){
+		res.insert(i * a + (N - 1 - i) * b);
+	}
+	for(int bil : res)
+		cout << bil << " ";
+	cout << endl;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	#ifdef LANO_DEBUG
+		cout << "DEBUG IS ON" << endl;
+		freopen("dbg.txt","w",stderr);
+	#endif
+	int T;cin >> T;
+	while(T--)
+		solve();
+	return 0;
+}
+
+
+
+
+
